d411cf7dea82883d6064bb8d2a6f9715a206939c
Mateus Pimentel <mateus.pimentel.w@gmail.com>
2020-08-20 15:10:29 -0300

    Tipos de dados e operações, parcial

---
+++ dadosBasicos.js
@@ -0,0 +1,85 @@
+
+
+
+// Textos, também conhecidos como Strings na programação
+
+let nome = 'Doguinho' // Texto entre aspas simple
+let nomeDoMeio = "Peidão" // Texto entre aspas duplas
+let ultimoNome = `da Silva` // Texto entre crases.
+
+// Números
+
+let pi = 3.1415 // Números decimais tem seus dígitos separados por ponto "."
+let cybo = 13
+let kelvin = -273
+
+// Booleanos ( Valores que representam verdadeiro ou falso [ True or False ] )
+
+let oCeuÉAzul = true
+let aLuaÉJacaré = false
+
+// Listas, também conhecidas como Arrays na programação
+
+let numerosDeUmÀDez = [1,2,3,4,5,6,7,8,9,10]
+let frutas = ["Abacate", "Açaí", "Laranja", "Pera"]
+let dadosMisturados = ["Texto", 1,2,3, [], {}, true, false]
+
+
+// Tabelas de associação
+
+/* 
+    Também conhecidos como "objetos" no javascript,
+    "dicionário" no python, "tabela" no PHP e "HashMap" no C++ 
+
+    São estruturas de dados que nos permitem armazenar várias informações 
+    nomeadas dentro de um mesmo lugar, exemplos à seguir:
+*/
+
+
+let pet = {
+    nome: "Jacaré",
+    sobrenome: "Bandeira",
+    idade: 246,
+    nacionalidade: "Tupinambá",
+    iventário: ["batata", "cenoura", "jabuticaba"]
+}
+
+
+let pessoa = {
+    nome: "Ximbinha",
+    sobrenome: "dos Teclados",
+    idade: undefined,
+    nacionalidade: "Belém do pará meu país",
+    comidaFavorita: "Açaí"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
